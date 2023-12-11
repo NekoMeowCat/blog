d234882b71b745722ee176d34bad361ef6742ad8
Jhonieeeeel. <123554139+JhonielPV@users.noreply.github.com>
2023-12-11 21:13:04 +0800

    cleaned html

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config("app.name", "Laravel") }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -13,11 +14,12 @@
         <script src="../path/to/flowbite/dist/datepicker.js"></script>
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(["resources/css/app.css", "resources/js/app.js"])
     </head>
+
     <body class="font-poppins antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+        <div class="min-h-screen bg-gray-800">
+            @include("layouts.navigation")
 
             <!-- Page Heading -->
             @if (isset($header))
@@ -34,4 +36,5 @@
             </main>
         </div>
     </body>
+
 </html>
--- resources/views/viewpost.blade.php
+++ resources/views/viewpost.blade.php
@@ -1,41 +1,50 @@
 <x-app-layout>
-    <section class="min-h-screen w-full border bg-black">
+    <section class="min-h-screen w-full   bg-gray-800">
         <div class="flex justify-center">
-            <div class="border-2 rounded-lg w-96 md:w-[600px] m-6">
+            <div class="border border-gray-500 rounded-lg w-96 md:w-[600px] m-6">
+
                 <!-- post profile -->
-                <div class="m-2 flex justify-start border rounded-md flex-grow">
+                <div class="m-2 flex justify-start border border-gray-500 rounded-md flex-grow">
                     <div class="m-2 border rounded-full w-16">
-                        <img class="h-full rounded-full object-cover w-full" src="{{ asset('storage/' . Auth::user()->image) }}" alt="Bordered avatar">
+                        <img class="h-full rounded-full object-cover w-full"
+                            src="{{ asset("storage/" . Auth::user()->image) }}" alt="Bordered avatar">
                     </div>
-                    <div class="w-5/6 border m-1">
+                    <div class="w-5/6 border-1 m-1">
                         <div class="flex justify-start items-start">
                             <span class="font-bold text-white"> {{ $post->author }}</span>
                         </div>
                         <div class="flex justify-start items-start">
-                            <small class="font-light font-poppins text-gray-400"> {{ $post->created_at->format('F d, Y \a\t H:i') }} </small>
+                            <small class="font-light font-poppins text-gray-400">
+                                {{ $post->created_at->format('F d, Y \a\t H:i') }} </small>
                         </div>
                     </div>
-                </div> 
+                </div>
                 <!-- post profile -->
+
                 <!-- post content -->
-                <div class="m-2 flex justify-start border rounded-md">
-                    <span class="text-start font-poppins text-gray-200 text-base">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum accusamus labore cumque adipisci, quos praesentium soluta, repellat asperiores veritatis aliquam excepturi vero dignissimos dolorem nihil saepe atque quia tempore nam doloribus! Laborum ut, accusantium, cupiditate ad doloribus dicta harum ipsam aliquid corporis placeat quam totam nam laudantium vel labore unde.
+                <div class="m-2 flex justify-start border border-gray-500 rounded-md">
+                    <span class="text-start font-poppins text-gray-200 text-base p-3">
+                        {{ $post->body }}
                     </span>
                 </div>
                 <!-- post content -->
+
                 <!-- post comment submit -->
-                <div class="m-2 flex border rounded-md">
-                    <form class="w-full" method="post" action="{{ route('comments.store') }}">
+                <div class="m-2 flex border-1 rounded-md">
+                    <form class="w-full" method="post" action="{{ route("comments.store") }}">
                         @csrf
                         <input type="hidden" id="post_id" name="post_id" value="{{ $post->id }}">
-                        <div class="w-full border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+                        <div
+                            class="w-full border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
                             <div class="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
                                 <label for="comment" class="sr-only">Your comment</label>
-                                <textarea id="comment" rows="4" class="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Write a comment..." name="comment" required></textarea>
+                                <textarea id="comment" rows="4"
+                                    class="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
+                                    placeholder="Write a comment..." name="comment" required></textarea>
                             </div>
                             <div class="flex items-center justify-end px-3 py-2 border-t dark:border-gray-600">
-                                <button type="submit" class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
+                                <button type="submit"
+                                    class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
                                     Post comment
                                 </button>
                             </div>
@@ -43,27 +52,29 @@
                     </form>
                 </div>
                 <!-- post comment submit -->
+
                 <!-- post comment section -->
                 <div class="m-2 block border rounded-md h-auto">
                     @foreach ($comments as $comment)
                         <div class="flex items-start w-full mr-10 p-2 mb-2 bg-gray-700 rounded ">
                             <div class="m-1 border rounded-full w-14">
-                                <img class="h-14 rounded-full object-cover w-full" src="{{ asset('storage/' . Auth::user()->image) }}" alt="Bordered avatar">
+                                <img class="h-14 rounded-full object-cover w-full"
+                                    src="{{ asset("storage/" . Auth::user()->image) }}" alt="Bordered avatar">
                             </div>
                             <div class="w-5/6 border m-1">
                                 <div class="flex justify-start items-start">
                                     <span class="font-bold text-white"> {{ $comment->user->name }}</span>
                                 </div>
                                 <div class="flex justify-start items-start">
-                                    <small class="font-light font-poppins text-gray-400"> {{ $post->created_at->format('F d, Y \a\t H:i') }} </small>
+                                    <small class="font-light font-poppins text-gray-400">
+                                        {{ $post->created_at->format('F d, Y \a\t H:i') }} </small>
                                 </div>
                                 <div class="">
                                     <span class="text-gray-200">{{ $comment->content }}</span>
                                 </div>
-                            </div>                          
+                            </div>
                         </div>
-
-                    @endforeach   
+                    @endforeach
                 </div>
                 <!-- post comment section -->
             </div>
@@ -84,5 +95,3 @@
         reader.readAsDataURL(file);
     }
 </script>
-
-
