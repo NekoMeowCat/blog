6e9ecff01d367bb2428806367092804cb244f565
Junzel Dela Torre <107986856+NekoMeowCat@users.noreply.github.com>
2023-12-11 21:24:33 +0800

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,7 +15,7 @@
             @foreach ($posts as $post)
                 <div class="card">
                     <a  href="{{ route('viewpost', ['id' => $post->id ]) }}" class=" flex flex-col items-start bg-white border-4 border-gray-200 rounded-lg shadow md:flex-row mb-3  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="{{ asset('storage/images/default.jpg') }}" alt="">
+                        <img class="object-contain w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="{{ asset('storage/' . $post->image) }}" alt="">
                         <div class="flex w-full flex-col justify-between p-4 leading-normal">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $post->user->name }}</h5>
                             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $post->body }}</p>
