942a0cf83933143709f1110d010b025650a396e1
Jhonieeeeel. <123554139+JhonielPV@users.noreply.github.com>
2023-12-21 19:58:37 +0800

    Update Password issue Fixed

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\ProfileUpdateRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
 
@@ -25,43 +26,49 @@ class ProfileController extends Controller
      * Update the user's profile information.
      */
     public function update(Request $request)
-{
-    $user = Auth::user();
+    {
+        $user = Auth::user();
 
-    $request->validate([
-        'name' => 'required|string|max:255',
-        'biography' => 'required|string|max:255',
-        'current_password' => 'nullable|string|password',
-        'password' => 'nullable|string|min:8|confirmed',
-        'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048', // Add validation for the image file
-    ]);
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'biography' => 'required|string|max:255',
+            'current_password' => 'nullable|string',
+            'password' => 'nullable|string|min:8|confirmed',
+            'profile_image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048', // Add validation for the image file
+        ]);
 
-    $user->name = $request->name;
-    $user->biography = $request->biography;
+        $user->name = $request->name;
+        $user->biography = $request->biography;
 
-    if ($request->hasFile('profile_image')) {
-        $profileImage = $request->file('profile_image');
-        $imagePath = $profileImage->store('images', 'public');
-        $user->image = $imagePath;
+        if ($request->hasFile('profile_image')) {
+            $profileImage = $request->file('profile_image');
+            $imagePath = $profileImage->store('images', 'public');
+            $user->image = $imagePath;
+        }
+
+        if ($request->filled('current_password')) {
+            // Check if the entered current password is correct
+            if (!Hash::check($request->current_password, $user->password)) {
+                return back()->withErrors(['current_password' => 'The current password is incorrect.']);
+            }
+
+            // Change the password only if a new password is provided
+            if ($request->filled('password')) {
+                $user->password = Hash::make($request->password);
+            }
+        }
+
+        $user->save();
+
+        return back()->with('status', 'profile-updated');
     }
-
-    if ($request->filled('password')) {
-        $user->password = Hash::make($request->password);
-    }
-
-    $user->save();
-
-    return back()->with('status', 'profile-updated');
-}
 
     public function show()
     {
         $user = Auth::user();
-        
-        return view('viewprofile');
+
+        return view('viewprofile', compact('user'));
     }
-
-
 
     /**
      * Delete the user's account.
--- resources/views/add-post.blade.php
+++ resources/views/add-post.blade.php
@@ -1,35 +1,48 @@
 <x-app-layout>
     <section class="min-h-screen overflow-hidden bg-gray-800">
-        <div class="m-8 md:mx-14 border rounded-sm">         
-            <div class="border rounded-md m-1">
+        <div class="m-8 md:mx-14 p-3 rounded-sm">
+            <div class="rounded-md m-1">
                 <span class="text-lg text-gray-100">What's on your mind?</span>
             </div>
-            <form method="post" action="{{ route('post.addPost') }}" enctype="multipart/form-data">
+            <form method="post" action="{{ route("post.addPost") }}" enctype="multipart/form-data">
                 @csrf
-                <div class="block md:flex m-1 rounded-md border">
-                    <div class="border m-1 md:w-1/2">
+                <div class="block md:flex m-1 rounded-md">
+                    <div class=" p-2 m-1 md:w-1/2">
                         <div class="w-full flex justify-center">
-                            <img id="preview-image" class="h-full uploaded-image rounded-lg" src="{{ asset('storage/images/picture.png') }}" alt="">
+                            <img id="preview-image" class="h-full uploaded-image rounded-lg"
+                                src="{{ asset("storage/images/picture.png") }}" alt="">
                         </div>
-                        <input id="post_image" name="post_image" type="file" class="hidden" accept="image/*" onchange="handleFileInputChange(event)" />
-                        <label for="post_image" class="mt-7 inline-flex items-center text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+                        <input id="post_image" name="post_image" type="file" class="hidden" accept="image/*"
+                            onchange="handleFileInputChange(event)" />
+                        <label for="post_image"
+                            class="mt-7 inline-flex items-center text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
                             Upload Photo
                         </label>
                     </div>
-                    <div class="w-full px-8">
+                    <div class="w-full py-2 px-8">
                         <div class="mb-6">
-                    <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Post Title</label>
-                    <input type="text" id="blog-title" name="blog-title" class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-teal-500 focus:border-teal-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-teal-500 dark:focus:border-teal-500" required />
-                </div>
-                <div class="mb-6">
-                    <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Content</label>
-                    <textarea id="blog-content" name="blog-content" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-teal-500 focus:border-teal-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-teal-500 dark:focus:border-teal-500" placeholder="Write your thoughts here..." required></textarea>
-                </div>
-                <div class="mb-4">
-                    <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Author ( Optional )</label>
-                    <input type="text" id="blog-author" name="blog-author" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-teal-500 focus:border-teal-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-teal-500 dark:focus:border-teal-500">
-                </div>
-                <button type="submit" class="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Submit</button>
+                            <label for="default-input"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Post Title</label>
+                            <input type="text" id="blog-title" name="blog-title"
+                                class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-teal-500 focus:border-teal-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-teal-500 dark:focus:border-teal-500"
+                                required />
+                        </div>
+                        <div class="mb-6">
+                            <label for="message"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Content</label>
+                            <textarea id="blog-content" name="blog-content" rows="4"
+                                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-teal-500 focus:border-teal-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-teal-500 dark:focus:border-teal-500"
+                                placeholder="Write your thoughts here..." required></textarea>
+                        </div>
+                        <div class="mb-4">
+                            <label for="default-input"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Author ( Optional
+                                )</label>
+                            <input type="text" id="blog-author" name="blog-author"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-teal-500 focus:border-teal-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-teal-500 dark:focus:border-teal-500">
+                        </div>
+                        <button type="submit"
+                            class="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Submit</button>
                     </div>
                 </div>
             </form>
@@ -41,7 +54,7 @@
     #preview-image {
         height: 300px;
         width: 350px;
-        object-fit:cover;
+        object-fit: cover;
     }
 </style>
 
@@ -58,5 +71,3 @@
         reader.readAsDataURL(file);
     }
 </script>
-
-
